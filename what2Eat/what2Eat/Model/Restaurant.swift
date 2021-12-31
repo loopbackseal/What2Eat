07d8d23bf56324ea0ff1e1ad1d68bd7ffeb7a955
loopbackseal <young961027@gmail.com>
2021-12-31 14:09:30 +0900

    Korean Localization CFBundleDisplayName

---
+++ what2Eat/what2Eat/Model/Restaurant.swift
@@ -0,0 +1,20 @@
+//
+//  Restaurant.swift
+//  what2Eat
+//
+//  Created by Young Soo Hwang on 2021/12/31.
+//
+
+import Foundation
+
+let restaurant = [
+    "강강술래",
+    "가리탕집",
+    "집밥",
+    "생고기짜글이",
+    "쉑쉑",
+    "서오릉피자",
+    "알떡",
+    "돈까스",
+    "다시 하시겠습니까?"
+]
